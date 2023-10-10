cb5607947f87da456e66a0faed468f28309a5934
Mykyta <mykyta.makarenko@symphony-solutions.com>
2023-10-10 16:22:17 +0300

    SQL Finished Hometask

---
+++ SQL/SQL 1.sql
@@ -0,0 +1,32 @@
+
+--Create query to GET  top 5 cities with highest number of Employees in it.
+SELECT COUNT(OrderID) as Orders, CustomerID FROM Employees
+GROUP BY CustomerID
+ORDER BY Orders DESC
+LIMIT 5;
+
+
+--Create query to GET top 5 customers with highest number of Orders.
+
+SELECT COUNT(OrderID) as Orders, CustomerName FROM Orders
+JOIN Customers C on O.CustomerID=C.CustomerID
+GROUP BY O.CustomerID
+ORDER BY Orders DESC
+LIMIT 5;
+
+
+--Create query to GET top 5 customers with biggest total quantity of items from all orders.
+
+SELECT SUM(Quantity) as Total_Items, CustomerName FROM Orders O
+JOIN OrderDetails OD on O.OrderID=OD.OrderID
+JOIN Customers C on O.CustomerID=C.CustomerID
+GROUP BY CustomerName
+ORDER BY Total_Items DESC
+LIMIT 5;
+
+
+
+--Create query to GET all product and category information at the same query
+
+SELECT * FROM Products P
+JOIN Categories C ON P.CategoryID=C.CategoryID;
---
+++ SQL/SQL 2.sql
@@ -0,0 +1,61 @@
+--Table 1: Movies
+
+Create TABLE Movies (
+	MovieID int NOT NULL,
+	Title varchar(255),
+	ReleaseYear int NOT NULL,
+	DurationMovies int NOT NULL,
+	Primary Key (MovieID) 
+);
+
+--Table 2: Cinema Halls 
+
+Create TABLE CinemaHalls (
+	HallID int NOT NULL,
+	HallName varchar(255),
+	Seating Capacity int,
+	PRIMARY KEY (HallID) 
+);
+
+--Table 3: Showtimes 
+
+Create TABLE Showtimes (
+	ShowtimeID int NOT NULL,
+	MovieID int NOT NULL,
+	HallID int NOT NULL, 
+	ShowtimeDateTime timestamp,
+	TicketPrice int, 
+	PRIMARY KEY (ShowtimeID),
+	FOREIGN KEY (MovieID) REFERENCES Movies(MovieID),
+	FOREIGN KEY (HallID) REFERENCES CinemaHalls(HallID)
+);
+
+
+--Table 4: Tickets 
+
+Create TABLE Tickets (
+	TicketID int NOT NULL,
+	ShowtimeID int NOT NULL,
+	SeatNumber int NOT NULL,
+	CustomerName varchar(255),
+	PurchaseDateTime timestamp,
+	Primary Key (TicketID), 
+	FOREIGN KEY (ShowtimeID) REFERENCES Showtimes(ShowtimeID)
+);
+
+-- Seed data into this table (INSERT)
+
+INSERT INTO Movies (MovieID, Title, ReleaseYear, DurationMovies)
+VALUES 
+('228', 'Drive', '2011', '100'), 
+('1337', 'Interstellar', '2013', '169');
+
+-- Update raw
+
+UPDATE Movies
+SET ReleaseYear = 2014
+WHERE MovieID = 228;
+
+-- Delete raw 
+Delete FROM Movies 
+WHERE MovieID = 228;
